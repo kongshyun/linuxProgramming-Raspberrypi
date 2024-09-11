9e0d1132039d4974b04a5607a6de3f76f4a0bf6c
kongshyun <tlgus927@naver.com>
2024-09-11 14:01:58 +0900

    [3차 14:01]

--- Mini/server.c
+++ Mini/server.c
@@ -9,25 +9,12 @@
 
 #define TCP_PORT 5100 /*서버의 포트 번호*/
 
-/*로그인 구조체*/
-typedef struct{
-    int sockfd;         //클라이언트 소켓 파일 디스크립터 
-    char username[50];  //클라이언트 사용자명
-    int is_logged_in;   // 로그인 상태(1:로그인, 2:로그아웃)
-}Client;
-
-/*메세지 구조체*/
-typedef struct {
-    int from_id;        // 발신자 클라이언트ID
-    char content[256];  // 메시지내용
-}Message;
-
 int main(int argc, char **argv)
 {
     int ssock;
 //    int csock;
     socklen_t clen;  /*소켓 디스크립터 정의 */
-    pid_t childpid; // 자식 프로세스 ID
+    pid_t pid;
     int n;
     struct sockaddr_in servaddr, cliaddr;/*주소 구조체 정의 */
 
@@ -40,7 +27,6 @@ int main(int argc, char **argv)
     }
     
     printf("Server Socket is created.! \n");
-
     /*주소 구조체에 주소 지정 */
     memset(&servaddr, 0,sizeof(servaddr));
     servaddr.sin_family=AF_INET;
@@ -52,43 +38,47 @@ int main(int argc, char **argv)
         return -1;
     }
     /*대기하는 클라이언트 숫자 설정. */
-    if(listen(ssock,10)<0){ // 수정 
+    if(listen(ssock,8)<0){
         perror("listen()");
         return -1;
     }
     clen=sizeof(cliaddr);
     do {
-        int n;
-        int csock = accept(ssock,(struct sockaddr *)&cliaddr, &clen);
+        int n, csock = accept(ssock,(struct sockaddr *)&cliaddr, &clen);
         /*클라이언트를 문자열로 변경 */
         
         inet_ntop(AF_INET, &cliaddr.sin_addr,mesg,BUFSIZ);
         printf("Client is connected : %s\n",mesg);
         /*자식프로세스 생성 */
-        if((childpid=fork())<0){
+        if((pid=fork())<0){
             perror("Error");
         
-        }else if (childpid==0){
-
-            close(ssock); //서버 소켓 ID를 닫는다. 
+        }else if (pid==0){
+            close(ssock); /*서버 소켓을 닫음*/  
             do{
                 memset(mesg,0,BUFSIZ);
-                if ((n=read(csock, mesg, BUFSIZ))<=0)
-                     perror("read()");
-                     printf("Received data : %s",mesg);
-
+                if ((n=read(csock, mesg, BUFSIZ))<=0) {
+                    perror("read()");
+                    break;
+                }
+                printf("Received data : %s",mesg);
+                
+                /*종료조건 추가*/
+                if(strncmp(mesg,"q",1)==0){
+                    printf("Client requested to close connection.\n");
+                    break;
+                }
 
                 /*클라이언트로 buf에 있는 문자열 전송 */
-                if(write(csock, mesg, n) <=0)
+                if(write(csock, mesg, n) <=0){
                     perror("write()");
-            } while(strncmp(mesg,"q",1));
+                    break; // 오류발생시 루프 종료 
+                }
+            } while(1);
     
-        close(csock);/* 클라이언트 소켓을 닫음*/
-        exit(0);
-        
-        
+            close(csock);/* 클라이언트 소켓을 닫음*/
+            exit(0);    
         }
-    //    wait(&status); //세기님 추가 
         close(csock);
     }while(strncmp(mesg,"q",1));
 
