55db0b58dcb4b4227ee7db564f86d8bf9e86b5e5
kongshyun <tlgus927@naver.com>
2024-10-14 12:22:54 +0900

    10.14 kernel GPIO Timer

---
+++ chapter12/GPIO_TIMER/.Module.symvers.cmd
@@ -0,0 +1,1 @@
+savedcmd_/home/iam/Desktop/veda-raspberrypi/chapter12/GPIO_TIMER/Module.symvers :=  scripts/mod/modpost -M -m -a      -o /home/iam/Desktop/veda-raspberrypi/chapter12/GPIO_TIMER/Module.symvers -T /home/iam/Desktop/veda-raspberrypi/chapter12/GPIO_TIMER/modules.order -i Module.symvers -e 
---
+++ chapter12/GPIO_TIMER/gpiotimer_module.mod
@@ -0,0 +1,1 @@
+/home/iam/Desktop/veda-raspberrypi/chapter12/GPIO_TIMER/gpiotimer_module.o
