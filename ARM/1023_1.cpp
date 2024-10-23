482286e4e8c4f6c981c61f0d890ad14114b054c8
kongshyun <tlgus927@naver.com>
2024-10-23 16:05:43 +0900

    1023 ARM

---
+++ ARM/1023_1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char * argv[])
+{
+    cout << "argc : " << argc << endl;
+    while(*argv) {
+        cout << *(argv++) << endl;
+    }
+    return 0;
+}
